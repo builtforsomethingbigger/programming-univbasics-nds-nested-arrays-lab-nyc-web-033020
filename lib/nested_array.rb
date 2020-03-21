e5dedd31ae2cafbcd4b6e77517f3fb5951522c46
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-21 17:46:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix(PRODUCE_STORAGE_ROOM)
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
-  PRODUCE_STORAGE_ROOM = [
+  [
     CONVENTIONAL_PRODUCE,
     ORGANIC_PRODUCE
   ]
